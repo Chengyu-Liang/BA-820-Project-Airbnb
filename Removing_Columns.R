ff37be20e750f02e16cb815c4af68214299d1bc7
Mansi Tolia <mtolia@bu.edu>
2019-11-19 16:13:06 -0500

    PCA done - Mansi

--- Removing_Columns.R
+++ Removing_Columns.R
@@ -1,5 +1,12 @@
 library(readr)
 library(tidyverse)
+library(dummies)
+library(fastDummies)
+library(factoextra)
+library(skimr)
+library(corrplot)
+library(purrr)
+library(cluster)
 
 reviews <- read_csv("reviews.csv")
 
@@ -57,12 +64,50 @@ saveRDS(listings2,file="Airbnb_listings.rds")
 
 airbnb_listings <- readRDS("Airbnb_listings.rds")
 
+airbnb_listings$price = as.numeric(gsub("[\\$,]", "", airbnb_listings$price))
+
+airbnb_listings$security_deposit = as.numeric(gsub("[\\$,]", "", airbnb_listings$security_deposit))
+
+airbnb_listings$cleaning_fee = as.numeric(gsub("[\\$,]", "", airbnb_listings$cleaning_fee))
+
+airbnb_listings$extra_people = as.numeric(gsub("[\\$,]", "", airbnb_listings$extra_people))
+
+listings_num = airbnb_listings %>% 
+  select_if(is.numeric) %>% 
+  select(-host_id, -id)
+
+listings_num[is.na(listings_num)] = 0
+
+summary(listings_num)
 
 
 
+airbnb_dummy <- airbnb_listings %>% 
+  select(room_type, property_type, cancellation_policy, host_response_time)
 
+airbnb_dummy[is.na(airbnb_dummy)] = 0
 
+airbnb_dummy <-dummy_cols(airbnb_dummy, remove_first_dummy = T) %>% 
+  select(-1:-4)
 
+listings_uml <- cbind(scale(listings_num), airbnb_dummy)
 
+listings_uml_cor <- cor(listings_uml)
 
+corrplot(listings_uml_cor, 
+         method = "color", 
+         diag=F, 
+         addCoef.col = "black")
 
+p = prcomp(listings_uml, center=T, scale = T)
+
+fviz_screeplot(p, addlabels=T, ylim =c(0,100))
+
+get_eigenvalue(p)
+
+fviz_pca_contrib(p, choice="var")
+fviz_pca_contrib(p, choice="var", axes = 2)
+
+s_pcs = predict(p, newdata = listings_uml)
+
+s_pcs = s_pcs[, 1:30]
